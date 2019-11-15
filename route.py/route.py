7276266df97e750e76351f4340e4042f7518b182
yashu446 <54535847+yashu446@users.noreply.github.com>
2019-11-15 16:31:10 +0530

    using templates

--- route.py/route.py
+++ route.py/route.py
@@ -1,9 +1,9 @@
-from flask import Flask
+from flask import Flask,render_template
 app = Flask(__name__)
 
 @app.route('/')
-def hello():
-    return "<h1></html>hello,unniverse<h1></html>"
+def index():
+    return render_template('index.html')
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0",port=3000,debug=True)

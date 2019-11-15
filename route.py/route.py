14767fce20c3c2b1d7fa839a41bd42b50e28c3ec
yashu446 <54535847+yashu446@users.noreply.github.com>
2019-11-15 16:39:31 +0530

    adding about file

--- route.py/route.py
+++ route.py/route.py
@@ -5,5 +5,9 @@ app = Flask(__name__)
 def index():
     return render_template('index.html')
 
+@app.route('/about')
+def about():
+    return render_template('about.html')
+        
 if __name__ == "__main__":
     app.run(host="0.0.0.0",port=3000,debug=True)

540b93b248aa3f64a57f8dd1640be7176b4f4d6d
yashu446 <54535847+yashu446@users.noreply.github.com>
2019-11-15 16:14:09 +0530

    adding html

--- route.py/route.py
+++ route.py/route.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return "Hello,world!"
+    return "<h1></html>hello,unniverse<h1></html>"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0",port=3000,debug=True)
